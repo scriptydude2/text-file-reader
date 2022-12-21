a52139550ab32cacc98b00e1f959c05f1c4d84aa
scriptydude2 <102865860+scriptydude2@users.noreply.github.com>
2022-12-21 15:53:38 +0700

    vex

--- src/commands.rs
+++ src/commands.rs
@@ -1,3 +1,4 @@
+use std::{fs::{File, self}};
 
 
 pub fn run(cmnd: String, raw_input: String) {
@@ -5,13 +6,26 @@ pub fn run(cmnd: String, raw_input: String) {
 
     match cmnd_raw {
         "create" => create_command(raw_input),
-        _ => println!("yoo")
+        "read" => read_command(raw_input),
+        _ => println!("error")
     }
 }
 
 //commands (private)
 
 fn create_command(_raw_input: String) {
+    let vec: Vec<&str> = _raw_input.split_whitespace().collect();
     
+    println!("///Operation: {cmd}, with the argument {arg}. Creating {arg}.txt",cmd = vec[0],arg = vec[1]);
+
+    let _file = File::create(format!("{name}.txt",name = vec[1]));
     
+}
+fn read_command(_raw_input: String) {
+    let vec: Vec<&str> = _raw_input.split_whitespace().collect();
+    println!("///Operation: {cmd}, with the argument {arg}. Reading {arg}.txt",cmd = vec[0],arg = vec[1]);
+
+    let contents = fs::read_to_string(format!("{}.txt", vec[1]))
+        .expect("Should have been able to read the file");
+    println!("||Reading {file}||\n{content}", file = vec[1], content = contents);
 }
